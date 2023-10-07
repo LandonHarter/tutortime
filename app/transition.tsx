01edc300c2b002c5666f9b917851f7b631509c74
LandonHarter <landonharter@outlook.com>
2023-10-06 22:57:14 -0400

    test

--- app/transition.tsx
+++ app/transition.tsx
@@ -6,6 +6,8 @@ import { usePathname } from "next/navigation"
 export default function PageTransition({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
 
+    return children;
+
     return (
         <AnimatePresence>
             <motion.div className='w-full h-full container' key={pathname} initial={{
